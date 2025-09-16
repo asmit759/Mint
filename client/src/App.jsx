1488009e0f4fff982b24148a3bf178b452e1840f
asmit759 <asmitsahu759@gmail.com>
2025-09-17 00:18:00 +0530

    added repo

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      hello wassup
+    </>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+
+app.listen(4000,()=>{
+    console.log( `App running at port no: ${4000}`);
+})
