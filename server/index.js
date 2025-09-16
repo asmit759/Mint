b309569fba3ad4f2a32b9e0a25524ec9709ffa6d
Mho <ommtripathi046@gmail.com>
2025-09-17 00:31:49 +0530

    MhoIn

--- server/index.js
+++ server/index.js
@@ -2,5 +2,5 @@ const express = require("express");
 const app = express();
 
 app.listen(4000,()=>{
-    console.log( `App running at port no: ${4000}`);
+    console.log( `App running at port number: ${4000}`);
 })
