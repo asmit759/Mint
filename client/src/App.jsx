6658f345319e1593ef8c1f98e3193b724b9d0895
asmit759 <asmitsahu759@gmail.com>
2025-10-16 00:38:35 +0530

    blanc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from "./components/Login"
 import StudentSignup from "./components/StudentSignup"
 import MentorSignup from "./components/MentorSignup"
+import MentorNavbar from './components/mentor/MentorNavbar'
 
 function App() {
   return (
@@ -13,6 +14,12 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/student/signup" element={<StudentSignup />} />
         <Route path="/mentor/signup" element={<MentorSignup />} />
+      </Routes>
+
+
+      {/* mentor */}
+      <Routes>
+        <Route path="/mentor-landing"  element={<MentorNavbar/>}/>
       </Routes>
     </BrowserRouter>
   )
