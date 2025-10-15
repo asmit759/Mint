ea56d1ce9045392b0d1fbba1a19caddf8139ca70
Mho <ommtripathi046@gmail.com>
2025-10-15 23:37:47 +0530

    landing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from "./components/Login"
 import StudentSignup from "./components/StudentSignup"
 import MentorSignup from "./components/MentorSignup"
+import StudentLanding from './components/student/Landing'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/student/signup" element={<StudentSignup />} />
+        <Route path="/student/landing" element={<StudentLanding/>} />
         <Route path="/mentor/signup" element={<MentorSignup />} />
       </Routes>
     </BrowserRouter>
