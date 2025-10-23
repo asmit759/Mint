52cd83e424f7bb38470601ac53b0194b9535dd57
Mho <ommtripathi046@gmail.com>
2025-10-23 23:10:28 +0530

    GrievanceCampus-Added

---
+++ client/src/components/student/HostelGrievance.jsx
@@ -0,0 +1,12 @@
+// src/components/student/CampusGrievance.jsx
+import React from 'react';
+
+function HostelGrievance() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-gray-800 via-black to-indigo-700 text-white p-8">
+      <h1 className="text-3xl font-bold">Campus Grievance</h1>
+    </div>
+  );
+}
+
+export default HostelGrievance;
