8f22f13ca8b7879d1129fce85d52a73e2265e1bd
Mho <ommtripathi046@gmail.com>
2025-10-24 22:15:28 +0530

    Mentor-approval

--- server/routes/leaveRouter.js
+++ server/routes/leaveRouter.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const leaveRouter = express.Router();
 const {studMid}= require("../middleware/studentMiddleware");
-const {mentorMid}= require("../middleware/mentorMid")
+const {mentorMid,isMentor}= require("../middleware/mentorMid")
 const {requestLeave,renderParentForm,verifyParentApproval,approveLeave} = require("../controllers/leaveController")
 
 leaveRouter.post("/createLeave",studMid,requestLeave);
 leaveRouter.get("/parent-form",renderParentForm);
 leaveRouter.post("/verify-parent",verifyParentApproval);
-leaveRouter.post("/approve-leave-mentor",mentorMid,approveLeave);
+leaveRouter.post("/approve-leave-mentor",mentorMid,isMentor,approveLeave);
 
 module.exports = leaveRouter
