cb65df7b3d89d4b303e246b347f0303b226e82db
asmit759 <asmitsahu759@gmail.com>
2025-12-29 22:06:57 +0530

    student mentor link by admin is done

--- server/controllers/adminControllers.js
+++ server/controllers/adminControllers.js
@@ -1,5 +1,9 @@
 
 const jwt = require("jsonwebtoken")
+
+const mentor = require("../models/mentor");
+const student = require("../models/studentSchema");
+
 const adminLogin = async(req,res)=>{
     try{
         const {email,password} = req.body;
@@ -18,4 +22,64 @@ const adminLogin = async(req,res)=>{
     }
 }
 
-module.exports = {adminLogin};
+
+const assign = async (req,res)=>{
+    try {
+        
+        const {mentor:mentorEmail,student: studentEmailArr} = req.body;
+
+        if(!mentorEmail || studentEmailArr.length === 0){
+            return res.status(400).json({
+                message:"mentor email or student email is not entered"
+            })
+        }
+
+        const mentorData = await mentor.findOne({email:mentorEmail});
+        if(!mentorData){
+            return res.status(400).json({
+                message:"mentor not found"
+            })
+        }
+
+        const students = await student.find({
+            email_id: { $in: studentEmailArr },
+            mentor: null
+        });
+
+        // if(!students.length !== studentEmailArr.length){
+        //     return res.status(400).json({
+        //         message:"Some students not found or already assigned a mentor"
+        //     })
+        // }
+
+        
+        const studentIds = students.map(s => s._id);
+
+        await student.updateMany(
+            { _id: { $in: studentIds } },
+            { $set: { mentor: mentorData._id } }
+        );
+
+        await mentor.updateOne(
+            { _id: mentorData._id },
+            { $push: { mentees: { $each: studentIds } } }
+        );
+        
+
+        return res.status(200).json({
+            message: "Mentor assigned to students successfully",
+            mentor: mentorData.email,
+            studentsAssigned: studentIds.length
+            
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message:"some error occured",
+            error,
+        })
+    }
+}
+
+module.exports = {adminLogin,assign};
--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const adminRouter = express.Router();
 const {adminMid} = require("../middleware/adminMiddleware");
-const {adminLogin} = require('../controllers/adminControllers')
+const {adminLogin,assign} = require('../controllers/adminControllers')
 const {createHostel,getAllHostels,getHostelById} = require("../controllers/hostelControllers");
 
 
@@ -11,5 +11,9 @@ adminRouter.post("/Login",adminLogin),
 adminRouter.post("/createHostel",adminMid,createHostel);
 adminRouter.get("/allHostels",adminMid, getAllHostels);
 adminRouter.get("/hostel/:id",adminMid, getHostelById);
+
+//assign mentor-student
+
+adminRouter.post("/assign",adminMid,assign);
 
 module.exports = adminRouter;
