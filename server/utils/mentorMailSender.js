ebb11b18cf573c9e7fadba32d67f51cc279089fb
asmit759 <asmitsahu759@gmail.com>
2026-03-04 20:54:02 +0530

    transporter update

--- server/utils/mentorMailSender.js
+++ server/utils/mentorMailSender.js
@@ -5,11 +5,15 @@ const mentorMailSender = async (mentorEmailId, studentEmailArray, title, body) =
   try {
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
       },
+      debug: true,
+      logger: true,
     });
 
 
